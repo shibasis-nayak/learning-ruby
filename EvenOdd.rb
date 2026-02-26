0501d3be8947d9309307b14cb5dff560bd729255
Shibasis Nayak <shibasisnayak09@gmail.com>
2026-02-26 15:47:38 +0530

    adding EvenOdd program

---
+++ EvenOdd.rb
@@ -0,0 +1,12 @@
+# write a program to accept a number from the terminal print if it is even or odd.
+
+print("enter a number ")
+num = gets 
+number = num.to_i
+if(number % 2 == 0)
+    puts("number is even")
+else
+    puts("number is odd")
+
+#out put = number is even
+#          number is odd
