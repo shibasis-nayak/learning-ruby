6f2890dc8400854bb93b53e1414ca467bc896ddc
Shibasis Nayak <shibasisnayak09@gmail.com>
2026-03-11 11:47:54 +0530

    Add output

--- AccessorMethods .rb
+++ AccessorMethods .rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :age   # accessor methods
+  attr_accessor :name, :age 
 
   def initialize(name, age)
 @name = name
@@ -9,7 +9,11 @@ end
 
 p = Person.new("Rahul", 25)
 
-puts p.name        # getter
-p.age = 26         # setter
+puts p.name        
+p.age = 26        
 
 puts p.age
+
+# output:
+# Rahul
+# 26
