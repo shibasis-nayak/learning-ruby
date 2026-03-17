c2b3f7304051c2a60bfba8c93f6083fb2160b8e8
Shibasis Nayak <shibasisnayak09@gmail.com>
2026-03-17 11:47:36 +0530

    Add loop to print even and odd numbers from 1 to 10

---
+++ Forloop.rb
@@ -0,0 +1,14 @@
+for i in 1..10
+  if i % 2 == 0
+    puts "#{i} is even"
+  else
+    puts "#{i} is odd"
+  end
+end
+
+puts "Check completed"
+puts "Even and odd numbers printed"
+
+# output = 1 is odd
+# 2 is even
+# 3 is odd
